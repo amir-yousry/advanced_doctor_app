e615c49763f75589a7bc00bf8a9bf3a32ab6efac
Amir Yousry <mr.amiryousry@gmail.com>
2026-08-29 06:51:32 +0300

    Add Routes & AppRouter
    Establishes core app architecture and UI
    
    Replaces the default Flutter demo app with a structured DocApp. This commit lays the foundation for future development by:
    
    - Setting up an `AppRouter` for streamlined navigation.
    - Introducing placeholder UI screens for key features: Onboarding, Login, Signup, and Home.
    - Integrating `flutter_screenutil` for a responsive and scalable UI across devices.
    - Adding essential packages for state management (Bloc), dependency injection (GetIt), data serialization (Freezed, JSON Serializable), and network communication (Dio, Retrofit).
    - Preparing for local data persistence with `shared_preferences`.

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,42 @@
+import 'package:advanced_doctor_app/core/routes/routes.dart';
+import 'package:advanced_doctor_app/features/home/ui/home_screen.dart';
+import 'package:advanced_doctor_app/features/login/ui/login_screen.dart';
+import 'package:advanced_doctor_app/features/onboarding/onboarding_screen.dart';
+import 'package:advanced_doctor_app/features/signup/ui/signup_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    //final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const LoginScreen(),
+          ),
+        );
+      case Routes.signUpScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<SignupCubit>(),
+            child: const SignupScreen(),
+          ),
+        );
+      case Routes.homeScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => HomeCubit(getIt())..getSpecializations(),
+            child: const HomeScreen(),
+          ),
+        );
+      default:
+        return null;
+    }
+  }
+}
