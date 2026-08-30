e641ce874d64a04e402c3fcfaf25495175781301
Amir Yousry <mr.amiryousry@gmail.com>
2026-08-30 20:01:39 +0300

    - Create Doctors List View Item Widget.
    -Create Doctors List View Widget.
    
    -Create Doctors ShimmerLoading Widget.
    
    -Create Doctors Bloc Builder Widget.

---
+++ lib/features/home/ui/widgets/doctors_list/doctors_list_view_item.dart
@@ -0,0 +1,73 @@
+import 'package:advanced_doctor_app/core/theme/colors.dart';
+import 'package:advanced_doctor_app/core/theme/styles.dart';
+import 'package:advanced_doctor_app/features/home/data/models/specializations_response_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gap/flutter_gap.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DoctorsListViewItem extends StatelessWidget {
+  final Doctors? doctorsModel;
+  const DoctorsListViewItem({super.key, this.doctorsModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16.h),
+      child: Row(
+        children: [
+          CachedNetworkImage(
+            imageUrl:
+                "https://static.wikia.nocookie.net/five-world-war/images/6/64/Hisoka.jpg/revision/latest?cb=20190313114050",
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Shimmer.fromColors(
+                baseColor: ColorsManager.lightGray,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            imageBuilder: (context, imageProvider) => Container(
+              width: 110.w,
+              height: 120.h,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+              ),
+            ),
+          ),
+          Gap(16.w),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  doctorsModel?.name ?? 'Name',
+                  style: TextStyles.font18DarkBlueBold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Gap(5.h),
+                Text(
+                  '${doctorsModel?.degree} | ${doctorsModel?.phone}',
+                  style: TextStyles.font12GrayMedium,
+                ),
+                Gap(5.h),
+                Text(
+                  doctorsModel?.email ?? 'Email',
+                  style: TextStyles.font12GrayMedium,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
