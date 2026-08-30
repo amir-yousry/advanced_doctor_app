c8a655c0285472b1cf431852f64d8fe90bcbacdc
Amir Yousry <mr.amiryousry@gmail.com>
2026-08-30 17:49:29 +0300

    - Create Signup State.
    - Create Signup Cubit.
    
    - Generate Signup State Freezed.

---
+++ lib/features/signup/logic/signup_cubit.dart
@@ -0,0 +1,41 @@
+import 'package:advanced_doctor_app/core/network/api_result.dart';
+import 'package:advanced_doctor_app/features/signup/data/models/signup_request_body.dart';
+import 'package:advanced_doctor_app/features/signup/data/repo/signup_repo.dart';
+import 'package:advanced_doctor_app/features/signup/logic/signup_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignupCubit extends Cubit<SignupState> {
+  final SignupRepo _signupRepo;
+  SignupCubit(this._signupRepo) : super(const SignupState.initial());
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController passwordConfirmationController =
+      TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  void emitSignupStates() async {
+    emit(const SignupState.loading());
+    final response = await _signupRepo.signup(
+      SignupRequestBody(
+        name: nameController.text,
+        email: emailController.text,
+        phone: phoneController.text,
+        password: passwordController.text,
+        passwordConfirmation: passwordConfirmationController.text,
+        gender: 0,
+      ),
+    );
+    response.when(
+      success: (signupResponse) {
+        emit(SignupState.success(signupResponse));
+      },
+      failure: (error) {
+        emit(SignupState.error(message: error.apiErrorModel.message ?? ''));
+      },
+    );
+  }
+}
